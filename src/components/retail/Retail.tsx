b82a76509fcdaa635e36701ef1a792aa35c6c2c4
Gamma <221785314+Coder-Gamma@users.noreply.github.com>
2025-12-08 13:08:57 +0545

    Made the retail landing page fully responsive

--- src/components/retail/Retail.tsx
+++ src/components/retail/Retail.tsx
@@ -3,21 +3,28 @@ import articleBgImage from "../../assets/Brunello-3-1920x726.jpg";
 
 const Retail = () => {
   return (
-    <div className="bg-[rgb(233,233,228)] h-screen overflow-hidden ">
-      <div>
+    <div className="bg-[rgb(233,233,228)] min-h-screen overflow-x-hidden ">
+      {/* This div is going to be a wrapper to hindle positioning for desktop */}
+      <div className="relative">
         <section
-          className="relative h-[70vh] bg-cover bg-center bg-no-repeat max-w-[1900px]"
+          className="relative mx-4 h-[85vh] md:h-[70vh] bg-cover bg-center bg-no-repeat max-w-[1900px]"
           style={{ backgroundImage: `url(${articleBgImage})` }}
         >
           <div className="absolute inset-0 bg-white/10 rounded-2xl"></div>
-          <h1 className="md:absolute uppercase text-4xl md:text-8xl font-normal border-t border-gray-300 md:mt-25  text-white md:top-30 px-4 py-8">
-            Retail fit out <br /> specialists
-          </h1>
         </section>
+        <h1
+          className=" uppercase  font-[52] text-gray-600   text-5xl  px-4 py-4 mb-4
+         md:text-8xl md:font-[75] md:absolute  md:mt-25   md:top-30  md:text-white md:mx-4 "
+        >
+          Retail fit out <br className="hidden md:block" /> specialists
+        </h1>
       </div>
-      <div className="flex flex-col px-6 md:flex-row justify-between bg-[rgb(233,233,228)] pb-8 md:pb-10 pt-6 md:pt-2 gap-4 md:gap-0 min-h-[30vh] md:min-h-0 ">
-        <div className="max-w-xl">
-          <p className="text-xl md:py-2 font-light">
+      <div
+        className="flex flex-col px-4 justify-between bg-[rgb(233,233,228)]  gap-4  
+        md:flex-row md:gap-0 md:py-10  "
+      >
+        <div className="max-w-xl pb-4">
+          <p className="text-xl font-medium ">
             At Eden, we understand the power of well-designed retail
             environments. Whether you're launching a new store, refreshing an
             existing space or rolling out a full retail concept, we bring the
@@ -26,15 +33,15 @@ const Retail = () => {
           </p>
         </div>
 
-        <div className="flex items-start space-x-2">
+        <div className="flex items-start py-10 md:py-0 gap-4">
           <img
             src="https://edenbuild.co.uk/wp-content/uploads/2025/08/kings-trust-logo-1.svg"
             className="w-12 md:w-[72px] "
             alt="King's Trust Logo"
           />
-          <p className="mt-0 text-base md:text-2xl ">
+          <p className="text-xl md:text-2xl ">
             Official partner of the
-            <br /> King's Trust.
+            <br className="hidden md:block" /> King's Trust.
           </p>
         </div>
       </div>
