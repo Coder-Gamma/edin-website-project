0bfcb3a50dcd2fcdb1903b982cabf9e973d058c8
Gamma <221785314+Coder-Gamma@users.noreply.github.com>
2025-12-11 12:30:00 +0545

    Made the Featured Project Section Responsive

--- src/components/case-study/FeaturedCaseStudy.tsx
+++ src/components/case-study/FeaturedCaseStudy.tsx
@@ -1,36 +1,39 @@
 import React from "react";
 import featuredImage from "./images/new-castle/Newcastle31a.jpg";
+import { ArrowRight } from "lucide-react";
 
 const FeaturedCaseStudy = () => {
   return (
     <div>
-      <div className="flex flex-col md:flex-row bg-black text-white px-4 py-18 gap-20">
-        {/* LEFT SIDE KO  */}
-        <div className="order-1">
-          <span className="text-2xl inline-block mb-20">
-            Featured Case Study
-          </span>
-          <h1 className=" text-4xl md:text-9xl uppercase font-light">
-            lighthouse project, newcastle
-          </h1>
+      <div className="flex flex-col  bg-black text-white px-4">
+        <div className="flex justify-between my-16 w-full">
+          <p className="text-2xl inline-block">Featured Case Study</p>
+          <p className="text-4xl xl:text-6xl xl:pr-40">01</p>
         </div>
-
-        {/* RIGHT SIDE KO  */}
-        <div className="text-2xl min-w-64 flex flex-col gap-8 order-2">
-          <p className="text-6xl mb-8 ml-2 ">01</p>
-          <p className="border-b border-gray-200">Community Centre</p>
-          <p className="border-b border-gray-200">Grade II listed</p>
-
-          <p className="border-b border-gray-200">3,900 sq ft</p>
+        <div className="grid md:grid-cols-[6fr_1fr] gap-40 pb-20 ">
+          <div className="order-1">
+            <h1 className=" text-5xl md:text-5xl xl:text-7xl 2xl:text-9xl uppercase font-light">
+              lighthouse project, newcastle
+            </h1>
+          </div>
+          <div className=" text-2xl min-w-64 flex flex-col gap-8 order-2 ">
+            <p className="border-b border-gray-200 py-1">Community Centre</p>
+            <p className="border-b border-gray-200">Grade II listed</p>
+            <p className="border-b border-gray-200">3,900 sq ft</p>
+          </div>
         </div>
       </div>
 
-      <div>
+      <div className="h-screen w-full relative">
         <img
           src={featuredImage}
           alt=""
           className="w-full h-full object-cover"
         />
+        <div className="absolute top-0 left-0 flex gap-4 items-center pt-14 pl-6 cursor-pointer">
+          <p className="text-2xl text-white hover:underline">Read More</p>
+          <ArrowRight size={48} strokeWidth={1} className="text-white font-thin " />
+        </div>
       </div>
     </div>
   );
