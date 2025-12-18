c0335175258ad35ad0555af0280c5b535172a6aa
Gamma <221785314+Coder-Gamma@users.noreply.github.com>
2025-12-18 12:54:09 +0545

    added new elements and applied proper styles and layout to the component

--- src/components/case-study/CaseStudyProjects.tsx
+++ src/components/case-study/CaseStudyProjects.tsx
@@ -16,8 +16,11 @@ const CaseStudyProjects: React.FC<CaseStudyProjectsProps> = (props) => {
   return (
     <div>
       <div className="grid lg:grid-cols-2 xl:grid-cols-3 gap-10  mx-4 border-t-2 border-gray-400 py-20 mb-10">
+        <div className=" md:hidden text-end">
+          <p className="text-3xl md:text-6xl">{displayIndex}</p>
+        </div>
         {/* LEFT KO DIV */}
-        <div className="">
+        <div className="order-1">
           <div className="2xl:h-135 xl:h-120 ">
             <img
               src={props.projectImage}
@@ -28,7 +31,7 @@ const CaseStudyProjects: React.FC<CaseStudyProjectsProps> = (props) => {
         </div>
 
         {/* ----------------Middle Div-------------- */}
-        <div className="xl:flex xl:flex-col xl:gap-18 ">
+        <div className="xl:flex xl:flex-col xl:gap-18 order-2 lg:order-3 xl:order-2">
           <h3 className=" text-3xl md:text-6xl  ">{props.projectName}</h3>
           <div className="lg:flex gap-14 items-center cursor-pointer hidden">
             <p className="text-xl xl:text-2xl hover:underline ">Read More</p>
@@ -38,25 +41,32 @@ const CaseStudyProjects: React.FC<CaseStudyProjectsProps> = (props) => {
 
         {/* ---------------RIGHT SIDE KO DIV----------------- */}
 
-        <div className="">
-          <div className="flex justify-between">
-            <div className="flex flex-col text-2xl gap-10">
-              <p>{props.categoryName}</p>
-              <p>{props.projectDuration}</p>
-              <p>{props.projectArea}</p>
+        <div className="order-3 lg:order-2 xl:order-3 w-full">
+          <div className="flex justify-between ">
+            <div className="flex flex-col text-2xl gap-10 w-full ">
+              <div className="w-full">
+                <p className="py-2">{props.categoryName}</p>
+                <div className="block md:hidden border-t-2 border-gray-400 " />
+              </div>
+              <div>
+                <p>{props.projectDuration}</p>
+                <div className="block md:hidden border-t-2 border-gray-400 " />
+              </div>
+              <div>
+                <p>{props.projectArea}</p>
+                <div className="block md:hidden border-t-2 border-gray-400 " />
+              </div>
             </div>
-            <div>
+            <div className="hidden md:block">
               <p className="text-3xl md:text-6xl">{displayIndex}</p>
             </div>
           </div>
-          <div className="md:mt-14 xl:mt-40">
+          <div className="mt-8 md:mt-14 xl:mt-40">
             <p className="text-xl xl:text-2xl">{props.paragraph}</p>
           </div>
         </div>
-        <div className="flex gap-14 items-center cursor-pointer lg:hidden">
-          <p className="text-xl xl:text-2xl hover:underline ">
-            Read More talako
-          </p>
+        <div className="flex gap-14 items-center cursor-pointer order-4 lg:hidden">
+          <p className="text-xl xl:text-2xl hover:underline ">Read More</p>
           <ArrowRight size={48} strokeWidth={1} className=" font-thin " />
         </div>
       </div>
