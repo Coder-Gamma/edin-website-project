13c24b0b1b5174810edb3399a020be38066711c6
Gamma <221785314+Coder-Gamma@users.noreply.github.com>
2025-12-18 16:46:49 +0545

    UPdated the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
 import { Menu } from "lucide-react";
 import { Link } from "react-router-dom";
+import { ArrowRight } from "lucide-react";
 
 function Navbar() {
-  const [open, setOpen] = useState(false);
+  // const [open, setOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <>
       <nav
-        className={`flex top-0 left-0 w-full z-40 items-center justify-between fixed
-  p-4 md:p-4 py-6 md:py-4 transition-colors duration-300
-  ${open ? "bg-white md:bg-[rgb(233,233,228)]" : "bg-[rgb(233,233,228)]"}`}
+        className={`fixed  top-0 left-0 flex w-full z-50 items-center justify-between
+  p-4 md:p-4 py-6 md:py-4 transition-all duration-300 bg-[rgb(233,233,228)]`}
       >
         <div>
           <Link
@@ -24,7 +25,7 @@ function Navbar() {
             <img
               src="https://edenbuild.co.uk/wp-content/themes/eden/assets/img/logo.svg"
               alt="logo"
-              className="w-18 sm:w-16 md:w-20 h-auto relative z-999"
+              className="w-18 sm:w-16 md:w-20 h-auto relative"
             />
           </Link>
         </div>
@@ -37,10 +38,10 @@ function Navbar() {
             <p className="hidden md:block text-xl font-normal text-gray-600 cursor-pointer">
               088000 432 639
             </p>
-
+            {/* MENU ICON Wrapper  */}
             <div
-              className="cursor-pointer relative"
-              onClick={() => setOpen(!open)}
+              className="cursor-pointer relative z-100"
+              onClick={() => setIsMenuOpen((prev) => !prev)}
             >
               <Menu strokeWidth={3} className="w-10 h-10 md:w-12 md:h-12 " />
             </div>
@@ -48,70 +49,104 @@ function Navbar() {
 
           <hr className="hidden sm:block w-full border-t border-gray-600 mt-2" />
         </div>
-      </nav>
 
-      <div
-        className={`
-    fixed right-0 bg-white/95 transition-all duration-500
-
-    top-[90px] h-[calc(100vh-90px)] w-full
-
-
-    md:top-0 md:h-screen md:w-[50%]
-
-    ${open ? "translate-x-0" : "translate-x-full"}
-    md:z-[60] z-[40]
-  `}
-      >
-        <div className="p-10 w-[80%] gap-8 text-3xl px-30 font-medium text-gray-600 flex flex-col ">
-          <Link
-            onClick={() => setOpen(false)}
+        <div
+          className={`absolute top-0 right-0 items-start
+          flex flex-col bg-white/95 gap-6 w-full md:w-[50%] h-screen pt-20 text-3xl font-normal
+          transform transition-transform ${
+            isMenuOpen ? "opacity-100" : "opacity-0"
+          }
+          `}
+          style={{ transition: "transform 0.3s ease, opacity 0.3s ease-in" }}
+        >
+          {/* <Link
+            onClick={() => setIsMenuOpen(false)}
             to="/"
             className=" border-b-2 border-gray-400 uppercase py-1"
           >
             Home
-          </Link>
-          <Link to="/" className=" border-b-2 border-gray-400 uppercase py-1">
-            Case Studies
-          </Link>
-          <Link
-            onClick={() => setOpen(false)}
-            to="/"
-            className=" border-b-2 border-gray-400  uppercase py-1"
-          >
-            Gallery
-          </Link>
+          </Link> */}
+          <div className="flex gap-20 border-b-2 border-gray-400 mx-20 w-80">
+            <Link
+              onClick={() => setIsMenuOpen(false)}
+              to="/"
+              className="  uppercase py-1"
+            >
+              Home
+            </Link>
+          </div>
+          <div className="flex items-center gap-20 border-b-2 border-gray-400 mx-20">
+            <Link
+              onClick={() => setIsMenuOpen(false)}
+              to="/case-study"
+              className="  uppercase py-1"
+            >
+              Case Studies
+            </Link>
+            <ArrowRight size={32} />
+          </div>
+          <div className=" border-gray-400 border-b-2 mx-20 w-80">
+            <Link
+              onClick={() => setIsMenuOpen(false)}
+              to="/gallery"
+              className="   uppercase py-1"
+            >
+              Gallery
+            </Link>
+          </div>
+          <div className=" border-gray-400 border-b-2 mx-20 w-80 flex items-center justify-between">
+            <Link
+              onClick={() => setIsMenuOpen(false)}
+              to="/about"
+              className="   uppercase py-1"
+            >
+              About
+            </Link>
+            <ArrowRight size={32} />
+          </div>
 
-          <Link
-            onClick={() => setOpen(false)}
-            to="/"
-            className=" border-b-2 border-gray-400 uppercase py-1"
-          >
-            Services
-          </Link>
-          <Link
-            onClick={() => setOpen(false)}
-            to="/"
-            className=" border-b-2 border-gray-400 uppercase py-1"
-          >
-            Sectors
-          </Link>
-          <Link
-            onClick={() => setOpen(false)}
-            to="/"
-            className=" border-b-2 border-gray-400 uppercase py-1 "
-          >
-            News
-          </Link>
-          <Link
-            onClick={() => setOpen(false)}
-            to="/"
-            className="  uppercase py-1"
-          >
-            Contact
-          </Link>
+          <div className=" border-gray-400 border-b-2 mx-20 w-80 flex items-center justify-between">
+            <Link
+              onClick={() => setIsMenuOpen(false)}
+              to="/services"
+              className="   uppercase py-1"
+            >
+              Services
+            </Link>
+            <ArrowRight size={32} />
+          </div>
+
+          <div className=" border-gray-400 border-b-2 mx-20 w-80 flex items-center justify-between">
+            <Link
+              onClick={() => setIsMenuOpen(false)}
+              to="/sectors"
+              className="   uppercase py-1"
+            >
+              Sectors
+            </Link>
+            <ArrowRight size={32} />
+          </div>
+
+          <div className=" border-gray-400 border-b-2 mx-20 w-80">
+            <Link
+              onClick={() => setIsMenuOpen(false)}
+              to="/gallery"
+              className="   uppercase py-1"
+            >
+              Gallery
+            </Link>
+          </div>
+          <div className=" border-gray-400 border-b-2 mx-20 w-80">
+            <Link
+              onClick={() => setIsMenuOpen(false)}
+              to="/contact"
+              className="   uppercase py-1"
+            >
+              Contact
+            </Link>
+          </div>
         </div>
-      </div>
+      </nav>
     </>
   );
 }
